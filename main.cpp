599be6d937317e48cc514b95e04ed07feb8a81e8
SofaL0rd <nazzar2303@gmail.com>
2021-10-29 19:22:41 +0300

    new original file

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "stdio.h" 
+#include "windows.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+using namespace std;
+
+int task1(int, int );
+
+int task2(int, int, float);
+
+int task3(int, int);
+
+int task1(int x, int y) {
+	int a;
+	a = (1 + pow(sin(x), 2)) / 2 + fabs((y - 3 * y)) / (1 + pow(x, 3) * pow(y, 2)) + sqrt(x) / pow((x - y), 1./3);
+
+	return a;
+}
+
+int task2(int x, int y, float alpha) {
+	int b;
+	b = pow(cos(tan(x + y)), 2) + sin((alpha - 270)/ M_PI);
+	return b;
+}
+
+int task3(int a, int b) {
+
+}
+
+int main()
+{
+ 
+}

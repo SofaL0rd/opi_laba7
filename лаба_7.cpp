d0468c2b25f7344b4dc835e0eaef6ae287fe4336
SofaL0rd <nazzar2303@gmail.com>
2021-11-11 20:25:51 +0200

    Changed file@

--- лаба_7.cpp
+++ лаба_7.cpp
@@ -1,21 +1,13 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include "stdio.h" 
-#include "windows.h"
 #include <iomanip>
 using namespace std;
 
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
- 
-
     float Z, m = 1, y;
-    cout << "Введіть y " << endl;
+    cout << " y " << endl;
     cin >> y;
     Z = 1;
     for (int i = 1; i < 11; i++) {
@@ -23,7 +15,5 @@ int main()
     }
     Z = (Z * cos(m * y)) + 2 * y;
     cout << "Z= " << setprecision(10) << Z << endl;
-    
-
 }
 

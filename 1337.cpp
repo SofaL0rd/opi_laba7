f642622163591094390bee8756f8d760bca1b738
SofaL0rd <nazzar2303@gmail.com>
2021-11-12 12:32:44 +0200

    chenged old ver

---
+++ 1337.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include "stdio.h" 
+#include "windows.h"
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+ 
+    float Z, m = 1, y;
+    cout << " y " << endl;
+    cin >> y;
+    Z = 1;
+    for (int i = 1; i < 11; i++) {
+        Z *= i;
+    }
+    Z = (Z * cos(m * y)) + 2 * y;
+    cout << "Z= " << setprecision(10) << Z << endl;
+    
+
+}
+
